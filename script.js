25668c05b501ee17bad1e0d7897af82521249efd
Marina <147822898+marixkh@users.noreply.github.com>
2025-08-27 09:45:06 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let countdown = document.getElementById('countdown');
+let birthDate = document.getElementById('birthday');
+let birthDateValue = birthDate.value;
+console.log(birthDate);
+function birthdayCounter()
+{
+    let todayDate = new Date();
+    let y = todayDate.getFullYear();
+    let m = todayDate.getMonth();
+    let d = todayDate.getDate();
+
+
+
+}
+birthdayCounter()
